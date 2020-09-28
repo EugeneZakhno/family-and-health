a2e2b64a95e28177abee849e419d351784c30bc9
EugeneZakhno <zohan1234567@gmail.com>
2020-09-28 21:24:52 +0300

    init

---
+++ src/main/java/com/herokuapp/familyandhealth/FamilyAndHealthApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp.familyandhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyAndHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyAndHealthApplication.class, args);
+	}
+
+}
